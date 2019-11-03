231ac4bf2d680da94f825fe1e906d467d76d0342
Ariel itzli Molina Sanchez <arielitzlimolinasanchez@MacBook-Pro-de-Ariel.local>
2019-11-03 01:10:01 -0600

    Styles "Working with styles for mobile version"

--- assets/scripts/mains.js
+++ assets/scripts/mains.js
@@ -1,3 +1,29 @@
 $(document).ready(function(){
   $('[data-toggle="tooltip"]').tooltip();
+
+  $("#mobile_button").click(function () {
+    $("#mobile_menu").toggleClass("active");
+  });
+  $(".hide_menu").click(function () {
+    $("#mobile_menu").toggleClass("active");
+  });
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 0,
+    items: 1,
+    nav: true,
+    dots: true,
+    responsiveClass: true
+  });
+  $(".owl-concepto").owlCarousel({
+    loop: true,
+    margin: 0,
+    items: 1,
+    autoplay: true,
+    autoplayTimeout: 1000,
+    autoplayHoverPause: true,
+    nav: true,
+    dots: true,
+    responsiveClass: true
+  });
 });
