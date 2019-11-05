f737e9ee7817821a7a68e1c53ae9531148f81044
itzli2000 <itzli2000@msn.com>
2019-11-05 09:48:41 -0600

    Changes "Working in changes asked by client"

--- assets/scripts/mains.js
+++ assets/scripts/mains.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function () {
   $('[data-toggle="tooltip"]').tooltip();
 
   $("#mobile_button").click(function () {
@@ -8,25 +8,34 @@ $(document).ready(function(){
     $("#mobile_menu").toggleClass("active");
   });
   try {
-  $(".owl-carousel").owlCarousel({
-    loop: true,
-    margin: 0,
-    items: 1,
-    nav: true,
-    dots: true,
-    responsiveClass: true
-  });
-  $(".owl-concepto").owlCarousel({
-    loop: true,
-    margin: 0,
-    items: 1,
-    autoplay: true,
-    autoplayTimeout: 1000,
-    autoplayHoverPause: true,
-    nav: true,
-    dots: true,
-    responsiveClass: true
-  });
-}
-catch(err) {}
+    $(".owl-carousel").owlCarousel({
+      loop: true,
+      margin: 0,
+      items: 1,
+      nav: true,
+      dots: true,
+      responsiveClass: true
+    });
+    $(".owl-concepto").owlCarousel({
+      loop: true,
+      margin: 0,
+      items: 1,
+      autoplay: true,
+      autoplayTimeout: 1000,
+      autoplayHoverPause: true,
+      nav: true,
+      dots: true,
+      responsiveClass: true
+    });
+  }
+  catch (err) { }
 });
+
+
+
+
+function scrollToElement(element) {
+  var elmnt = document.getElementById(element);
+  console.log(elmnt);
+  elmnt.scrollIntoView();
+}
