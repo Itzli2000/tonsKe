d40583d69fe42565c60587b5b9a62efac9191a73
itzli2000 <itzli2000@msn.com>
2019-12-18 11:42:44 -0600

    Refactor Add new method to send contact email

--- assets/scripts/mains.js
+++ assets/scripts/mains.js
@@ -152,3 +152,24 @@ function scrollToElement(element) {
   console.log(elmnt);
   elmnt.scrollIntoView();
 }
+
+var isMessageSent = setInterval(() => {
+  var urlActual = new URL(document.location);
+  var isSent = urlActual.searchParams.get("send");
+  if (isSent) {
+      console.log(isSent);
+      // window.history.replaceState({}, document.title, "/");
+      clearInterval(isMessageSent);
+      Swal.fire({
+          icon: 'success',
+          title: 'Contacto',
+          html: 'Su mensaje ha sido enviado, <br>Nos pondremos en contacto a la brevedad.',
+          confirmButtonColor: '#227a21'
+      }).then(response => {
+          document.location.href="/";
+      });
+  }
+  // else {
+  //     console.log('not sent');
+  // }
+}, 500);
---
+++ contact_mobile.php
@@ -0,0 +1,50 @@
+<?php
+if (isset($_POST['submit'])) {
+    $encoding = "utf-8";
+    // Preferences for Subject field
+    $subject_preferences = array(
+        "input-charset" => $encoding,
+        "output-charset" => $encoding,
+        "line-length" => 76,
+        "line-break-chars" => "\r\n"
+    );
+
+    // Multiple recipients
+    $to = 'informes@tons-ke.com';
+    $from_name = $_POST['name_mobile'];
+    $from_phone = $_POST['phone_mobile'];
+    $from_mail = $_POST['mail_mobile'];
+    $from_message = $_POST['message_mobile'];
+
+    // Subject
+    $subject = 'Contacto desde Tons-ke';
+
+    // Message
+    $message = '
+    <html>
+        <head>
+            <title>Mensaje.</title>
+        </head>
+        <body>
+            <h2>Mensaje enviado desde Tons-ke.</h2>
+            <h4>' . $from_name . ' <br>Correo: ' . $from_mail . ' <br>Teléfono: ' . $from_phone . ' <br>Escribió lo siguiente:</h4>
+            <p> Mensaje: ' . $from_message . '</p>
+        </body>
+    </html>
+    ';
+
+    // To send HTML mail, the Content-type header must be set
+    // Mail header
+    $header = "Content-type: text/html; charset=" . $encoding . " \r\n";
+    $header .= "De: " . $from_name . $from_lastname . " <" . $from_mail . "> \r\n";
+    $header .= "MIME-Version: 1.0 \r\n";
+    $header .= "Content-Transfer-Encoding: 8bit \r\n";
+    $header .= "Fecha: " . date("r (T)") . " \r\n";
+    $header .= iconv_mime_encode("Asunto", $mail_subject, $subject_preferences);
+
+    // Mail it
+    mail($to, $subject, $message, $header);
+
+    // Redirect
+    header('Location: index.html?send=true');
+}
